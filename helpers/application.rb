c4cebd4b4ed3b9927407ba3c459843c661e44131
Vinay Gupta <vinaybeta@yahoo.com>
2013-09-23 13:00:37 +0100

    updated mutliple files and incorporated changes posted by evgeny

--- helpers/application.rb
+++ helpers/application.rb
@@ -1,8 +1,5 @@
 helpers do
-	def cell_value(value)
-		value.to_i == 0 ? " " : value
-	end  
-
+	 
 	def colour_class(solution_to_check, puzzle_value, current_solution_value, solution_value)
 		must_be_guessed = puzzle_value == 0
 		tried_to_guess = current_solution_value.to_i != 0
@@ -17,4 +14,9 @@ helpers do
 			'value-provided'
 		end
 	end
+
+	def cell_value(value)
+		value.to_i == 0 ? " " : value
+	end 
+	
 end
--- sudoku.rb
+++ sudoku.rb
@@ -1,14 +1,13 @@
 require 'sinatra'
-require 'sinatra-partial'
-set :partial_template_engine, :erb
-require 'rack-flash3'
-use Rack::Flash
-set :session_secret, "I'm the secret key to sign the cookie"
+require 'sinatra/partial'
+require 'rack-flash'
 require_relative './lib/sudoku'
-require_relative './lib/cell'
 require_relative './helpers/application'
 
 enable :sessions
+set :session_secret, "I'm the secret key to sign the cookie"
+use Rack::Flash
+set :partial_template_engine, :erb
 
 # get '/' do
 # 	session[:last_visit] = Time.now.to_s
@@ -27,7 +26,7 @@ def random_sudoku
 end
 
 def puzzle(sudoku) #need to implement this
-	sudoku
+	sudoku.map {|v| rand < 0.3 ? 0 : v}
 end
 
 def generate_new_puzzle_if_necessary
@@ -46,6 +45,20 @@ def prepare_to_check_solution
 	session[:check_solution] = nil
 end
 
+def box_order_to_row_order(cells)
+	boxes = cells.each_slice(9).to_a
+	(0..8).to_a.inject([]) {|memo, i|
+		first_box_index = i / 3 * 3 
+		three_boxes = boxes[first_box_index, 3]
+		three_rows_of_three = three_boxes.map do |box|
+			row_number_in_a_box = i % 3
+			first_cell_in_the_row_index = row_number_in_a_box * 3
+			box[first_cell_in_the_row_index, 3]
+		end
+		memo += three_rows_of_three.flatten
+	}
+end
+
 get '/' do
 	prepare_to_check_solution
 	generate_new_puzzle_if_necessary
@@ -56,12 +69,12 @@ get '/' do
 end
 
 get '/solution' do
-	@current_solution = session[:solution]
+	@puzzle = session[:solution]
 	erb :index
 end
 
 post '/' do
-	cells = params["cell"]
+	cells = box_order_to_row_order(params["cell"])
 	session[:current_solution] = cells.map{|value| value.to_i}.join
 	session[:check_solution] = true
 	redirect to("/")
